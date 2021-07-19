da62fc3f9657e9e4227e0a311009a3f234606416
Rajasegar <rajasegar.c@gmail.com>
2021-07-19 16:50:39 +0530

    docs: update readme
    1. cleanup db bootstrap code from web.lisp
    2. change db path in config.lisp

--- README.markdown
+++ README.markdown
@@ -1,10 +1,31 @@
 # cl-computer-store
 
-
+A sample Computer store demo web application in Common Lisp
 
 ## Usage
+```lisp
+(ql:quickload :cl-computer-store)
+(cl-computer-store:start :port 3000)
+```
+
+Go to `http://localhost:3000` in your browser and see the app in action.
+
 
 ## Installation
+```
+cd ~/quicklisp/local-projects
+git clone https://github.com/rajasegar/cl-computer-store
+```
+
+## Creating SQLite db file from the schema
+You can generate a persistent database file like `computer-store.db` from the `db/schema.sql`
+
+```
+$ sqlite3
+sqlite3> .read db/schema.sql
+sqlite3> .save computer-store.db
+sqlite3> .quit
+```
 
 ## Author
 
--- src/web.lisp
+++ src/web.lisp
@@ -20,51 +20,6 @@
 (defvar *web* (make-instance '<web>))
 (clear-routing-rules *web*)
 
-;; Create manufacturers table
-;; (with-connection (db)
-;;   (execute
-;;    (drop-table :manufacturers :if-exists t))
-;;   (execute
-;;    (create-table :manufacturers
-;;        ((code :type 'integer
-;; 	      :primary-key t)
-;; 	(name :type 'string)))))
-
-;; Create products table
-;; (with-connection (db)
-;;   (execute
-;;    (drop-table :products :if-exists t))
-;;   (execute
-;;    (create-table :products
-;;        ((code :type 'integer
-;; 	      :primary-key t)
-;; 	(name :type 'string)
-;; 	(price :type 'integer)
-;; 	(manufacturer :type 'integer)))))
-
-;; Generate data for manufacturers
-;; (with-connection (db)
-;;   (execute (insert-into :manufacturers (set= :code 1 :name "Sony")))
-;;   (execute (insert-into :manufacturers (set= :code 2 :name "Creative Labs")))
-;;   (execute (insert-into :manufacturers (set= :code 3 :name "Hewlett-Packard")))
-;;   (execute (insert-into :manufacturers (set= :code 4 :name "Iomega")))
-;;   (execute (insert-into :manufacturers (set= :code 5 :name "Fujitsu")))
-;;   (execute (insert-into :manufacturers (set= :code 6 :name "Winchester")))
-;;   (execute (insert-into :manufacturers (set= :code 7 :name "Bose"))))
-   
-;; Generate data for products
-;; (with-connection (db)
-;;   (execute (insert-into :products (set= :code 1 :name "Hard drive" :price 240 :manufacturer 5)))
-;;   (execute (insert-into :products (set= :code 2 :name "Memory" :price 120 :manufacturer 6)))
-;;   (execute (insert-into :products (set= :code 3 :name "ZIP drive" :price 150 :manufacturer 4)))
-;;   (execute (insert-into :products (set= :code 4 :name "Floppy disk" :price 5 :manufacturer 6)))
-;;   (execute (insert-into :products (set= :code 5 :name "Monitor" :price 240 :manufacturer 1)))
-;;   (execute (insert-into :products (set= :code 6 :name "DVD drive" :price 180 :manufacturer 2)))
-;;   (execute (insert-into :products (set= :code 7 :name "CD drive" :price 90 :manufacturer 2)))
-;;   (execute (insert-into :products (set= :code 8 :name "Printer" :price 270 :manufacturer 3)))
-;;   (execute (insert-into :products (set= :code 9 :name "Toner cartridge" :price 66 :manufacturer 3)))
-;;   (execute (insert-into :products (set= :code 10 :name "DVD burner" :price 180 :manufacturer 2))))
-
 ;; ;;
 ;; Routing rules
 
